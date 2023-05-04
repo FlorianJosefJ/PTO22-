503dfd3aaee9fba7635685a9d51b6ee415b11359
FlorianJosefJ <132549320+FlorianJosefJ@users.noreply.github.com>
2023-05-04 11:37:40 +0200

    Add files via upload

--- Lab2 Grundlagen.py
+++ Lab2 Grundlagen.py
@@ -4,28 +4,15 @@ a = int(input("Zahl 1: "))
 b = int(input("Zahl 2: "))
 
 
-if b == 0:
+while b == 0:
     
-    print("Zahl 1 = 0!")
+
+  
     print("geben Sie eine neue Zahl für b ein (=ungleich 0)")
-    a = int(input("Zahl 1: "))
     b = int(input("Zahl 2: "))
-    print(a+b)
-    print(a*b)
-    print(a/b)
-    print(a-b)
-    
-else:
-    print(a+b)
-    print(a*b)
-    print(a/b)
-    print(a-b)
  
-
-
-
-
-
-
-
-
+    
+print(a+b)
+print(a*b)
+print(a/b)
+print(a-b)
